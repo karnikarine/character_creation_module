6f61361039207910d56768892af91ecb8ad2d1c1
Карина Бадалова <karnikarine@gmail.com>
2022-12-03 17:42:44 +0300

    add working version without classes

---
+++ testov.py
@@ -0,0 +1,25 @@
+from math import sqrt
+
+message = ('Добро пожаловать в самую лучшую программу для вычисления'
+           'квадратного корня из заданного числа')
+
+
+def calculate_square_root(number):
+    """Вычисляет квадратный корень."""
+    return sqrt(number)
+
+
+def calc(your_number):
+    """
+    Проверяет, явлеяется ли число положительным,
+    производит финальное вычисление и печатает результат.
+    """
+    if your_number <= 0:
+        return 'Число меньше или равно нулю'
+    root = calculate_square_root(your_number)
+    print(f'Мы вычислили квадратный корень из введённого вами числа.'
+          f'Это будет: {root}')
+    return root
+
+print(message)
+calc(25.5)
